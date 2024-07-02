0b9ba32310609df45ece2bf9d69c62dbc70a112f
taegyu-heo <165611198+taegyu-heo@users.noreply.github.com>
2024-07-02 13:09:50 +0900

    The answers to the Java quiz about variables
    "나도코딩"님의 유튜브의 자바 강의 기본편에 나온 퀴즈 중 1번 퀴즈에 대한 정답입니다.

---
+++ quiz01/src/quiz01.java
@@ -0,0 +1,13 @@
+public class quiz01 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        String busNum = "서울01";
+        int remaniMinute = 5;
+        float remaintDistance = 1.2F;
+
+        System.out.println(busNum + "번 버스");
+        System.out.println("약" + remaniMinute + "분 후 도착");
+        System.out.println("남은 거리" + remaintDistance + "km");
+    }
+}
